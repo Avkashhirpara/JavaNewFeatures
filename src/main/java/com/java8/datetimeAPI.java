7ff242e4bf2c0255c87ee63d7fa855e3bdd7147f
Avkash Hirpara <avkash.av1987@gmail.com>
2019-12-15 01:29:17 +0530

    date and time api joda. From java8

---
+++ src/main/java/com/java8/datetimeAPI.java
@@ -0,0 +1,42 @@
+package com.java8;
+
+import org.w3c.dom.ls.LSOutput;
+
+import java.time.*;
+
+public class datetimeAPI {
+
+    public static void main(String[] args) {
+        System.out.println("joda time api.");
+////////////////current system date and time /////////////////////////
+        System.out.println(LocalDate.now());
+        System.out.println(LocalTime.now());
+
+//////////////////////////////////////////////////////////////
+        LocalDate ld = LocalDate.now();
+        System.out.println("day of month >>"+ld.getDayOfMonth());
+        System.out.println("month value  >>"+ld.getMonthValue());
+        System.out.println("year         >>"+ld.getYear());
+        System.out.println("day of year >>"+ld.getDayOfYear());
+        System.out.println("day of week  >>"+ld.getDayOfWeek());
+/////////////////////////////////////////////////////////////////
+
+        System.out.println("custom day");
+        LocalDate ldt = LocalDate.of(1987, 5,12);
+        System.out.println("I was born on >>"+ldt.getDayOfWeek());
+
+        LocalDate ldtNow= LocalDate.now();
+        Period p = Period.between(ldt,ldtNow);
+        System.out.println("my age is "+ p.getYears()+" years "+ p.getMonths()+ "  months " +p.getDays()+" days");
+
+
+
+
+
+
+
+
+    }
+
+
+}
