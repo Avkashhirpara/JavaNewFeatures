b3ecee8073fe7ce93664819f5a7b032c0cd2532c
Avkash Hirpara <avkash.av1987@gmail.com>
2019-12-27 17:57:45 +0530

    add simplyfied version of distinct by to understand the logic.

--- src/main/java/com/java8/distinct/DistinctDemo.java
+++ src/main/java/com/java8/distinct/DistinctDemo.java
@@ -3,6 +3,7 @@ package com.java8.distinct;
 import com.practice.domain.Employee;
 import com.practice.domain.Student;
 
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -35,13 +36,16 @@ public class DistinctDemo {
         // generic distinc by will work on any list type
 
         System.out.println("=========================================================================");
-        System.out.println("Employee list without distict");
+        System.out.println("Employee list without distinct");
         List<Employee> employees = Employee.getList();
         System.out.println(employees);
         employees = distinctBy(employees,Employee::getRoll);
         System.out.println("distinct by the roll of employees ");
         System.out.println(employees);
-
+        employees = Employee.getList();
+        System.out.println("distinct by the employee salary");
+        employees = distinctBy(employees,Employee::getSalary);
+        System.out.println(employees);
     }
 
     public static <T,R>  List<T> distinctBy(List<T> list, Function<T,R> keyFunction){
@@ -49,6 +53,25 @@ public class DistinctDemo {
         List<T> newList = list.stream()
                 .filter(std -> setObject.add(keyFunction.apply(std)))
                 .collect(Collectors.toList());
+        return newList;
+    }
+
+    /**
+     * Simplified to understand the logic. This method only works with the student's list and the String
+     * type attributes.
+     * @param list
+     * @param keyFunction
+     * @return
+     */
+    public static  List<Student> simplyFiedDistinctBy(List<Student> list, Function<Student,String> keyFunction){
+        Set<Student> setObject = new HashSet<>();
+        List<Student> newList = new ArrayList<>();
+        for (Student t : list) {
+            if(! setObject.contains(keyFunction.apply(t))){
+                    setObject.add(t);
+                    newList.add(t);
+            }
+        }
         return newList;
     }
 
